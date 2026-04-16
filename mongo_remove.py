24cd387802f189a8954f74708231f815f8ddda70
JaneGovan <wenjingao69@163.com>
2026-04-16 10:33:38 +0800

    feat: mongo_remove

--- mongo_remove.py
+++ mongo_remove.py
@@ -1,5 +1,8 @@
 from utils.mongo import delete_user_and_records
+from argparse import ArgumentParser
 
 if __name__ == '__main__':
-    user_name = 'jane008'
-    delete_user_and_records(user_name)
+    parser = ArgumentParser(description="remove user record")
+    parser.add_argument("-u", "--username", default="", required=True, type=str, help="用户名，即用户ID")
+    config = parser.parse_args()
+    delete_user_and_records(config.username)
